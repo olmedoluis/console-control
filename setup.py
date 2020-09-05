209e0e7689a77fa3034773515d542047ece5c212
olmedoluis <63429352+olmedoluis@users.noreply.github.com>
2020-09-05 16:28:51 -0300

    [CC-2] Set up installation files (#3)
    * feat(install.sh):Create setup file
    
    * feat(setup.py):Create setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="console-control",
+    version="0.1.0",
+    packages=["console"],
+    author="Luis Olmedo",
+    author_email="olmedoluis012@gmail.com",
+    description="simple console manipulation interface",
+    project_urls={"Source Code": "https://github.com/olmedoluis/console-control",},
+)
+
